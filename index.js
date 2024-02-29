8ddd9d3ecc1fb36e2963607124328abaa0ee089a
natebobsee <nate.bob.see@gmail.com>
2024-02-29 13:45:26 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// === DO NOT EDIT THIS REGION ===
+// Read the comments to understand how the program is structured.
+
+// Prompt the user for a list of integers separated by commas.
+//const userInputString = prompt(
+   //"Please enter some integers separated by commas.",
+ //"1,2,3,4,5"
+//);
+userInputString = "Vanilla,Vanilla,Vanilla,Strawberry,Coffee,Coffee"
+// Split the string of numbers into an array of strings.
+const iceCreamType = userInputString.split(',');
+
+console.log(iceCreamType);
+ 
+function typetotal(iceCreamType){
+const amount = {};
+for (creamType of iceCreamType)
+{
+      if  (amount[creamType]===undefined)
+        amount[creamType]=1;
+      else 
+       amount[creamType]=amount[creamType]+1
+}
+      console.table(amount) ; 
+};
+typetotal(iceCreamType);
